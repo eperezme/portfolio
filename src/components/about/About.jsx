0f5eb71a2e5cdaa7edb8ff9a37eab59eb4288949
Viper <eduardperezmendez@gmail.com>
2023-12-01 00:55:53 +0100

    Add about section with image and icons

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/_MG_0114.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { FaProjectDiagram } from "react-icons/fa";
 
 function About() {
-	return <section id="about">About</section>;
+	return (
+		<section id="about">
+			<h5>Get To Know</h5>
+			<h2>About Me</h2>
+
+			<div className="container about__container">
+				<div className="about__me">
+					<div className="about__me-image">
+						<img src={ME} alt="About Image" />
+					</div>
+				</div>
+
+				<div className="about__content">
+					<div className="about__cards">
+						<article className="about__card">
+							<FaAward className="about__icon" />
+							<h5>Experience</h5>
+							<small>3+ Years Working</small>
+						</article>
+						<article className="about__card">
+							<FiUsers className="about__icon" />
+							<h5>Clients</h5>
+							<small>200+ Worldwide</small>
+						</article>
+						<article className="about__card">
+							<FaProjectDiagram className="about__icon" />
+							<h5>Projects</h5>
+							<small>80+ comleted projects</small>
+						</article>
+					</div>
+
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus aspernatur debitis, quas soluta nobis, atque et iusto vero fugit,
+						earum eius molestias quos tempora reiciendis maiores vel deserunt sapiente. Atque.
+					</p>
+
+					<a href="#contact" className="btn btn-primary">
+						Let's Talk
+					</a>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default About;
