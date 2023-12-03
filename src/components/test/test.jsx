6206ba663a4dfe499c0d71989bbf59363f717c2f
Viper <eduardperezmendez@gmail.com>
2023-12-03 07:50:27 +0100

    Refactor CSS styles and update card layout

--- src/components/test/test.jsx
+++ src/components/test/test.jsx
@@ -7,16 +7,16 @@ const test = () => {
   return (
     <section id="test" className='experience__class'>
       <h2>Test</h2>
-      <div className="container test__container">
-        <div className="test__card">
-          <h3><span className='highlight'>Lorem ipsum,</span> dolor sit amet consectetur adipisicing elit. Exercitationem explicabo, distinctio, quas quaerat sapiente earum nobis in ab omnis porro inventore ullam, laboriosam nam. Dolore quis cumque fuga assumenda dignissimos!</h3>
+      <div className="container vcard-container">
+        <div className="vcard">
+          <p><em>Lorem ipsum,</em> dolor sit amet consectetur adipisicing elit. Exercitationem explicabo, distinctio, quas quaerat sapiente earum nobis in ab omnis porro inventore ullam, laboriosam nam. Dolore quis cumque fuga assumenda dignissimos!</p>
 
           
           <img src={IMG1} alt=""/>
           
         </div>
-        <div className="test__card">
-        <h3><span className='highlight'>Lorem ipsum,</span> dolor sit amet consectetur adipisicing elit. Exercitationem explicabo, distinctio, quas quaerat sapiente earum nobis in ab omnis porro inventore ullam, laboriosam nam. Dolore quis cumque fuga assumenda dignissimos!</h3>
+        <div className="vcard">
+        <p><em>Lorem ipsum,</em> dolor sit amet consectetur consectetur consectetur consectetur </p>
           <img src={IMG2} alt="" />
         </div>
       </div>
