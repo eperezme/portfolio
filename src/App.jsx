74ce1441762fcf25924cbd090f17389c12f6160a
Viper <eduardperezmendez@gmail.com>
2023-12-05 21:25:03 +0100

    starting to build

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 import React from "react";
 import Header from "./components/header/Header";
-import Nav from "./components/nav/Nav";
-import About from "./components/about/About";
-import Experience from "./components/experience/Experience";
-import Portfolio from "./components/portfolio/Portfolio";
-import Contact from "./components/contact/Contact";
-import Footer from "./components/footer/Footer";
-import Services from "./components/services/Services";
-import Test from "./components/test/test";
 
 // [ ] Make a vertical DNA strand that unfolds as you scroll down
 
@@ -15,14 +7,6 @@ const App = () => {
 	return (
 		<>
 			<Header />
-			{/* <Nav /> */}
-			<About />
-			<Experience />
-			<Services />
-			<Portfolio />
-			<Contact />
-			<Footer />
-			<Test />
 		</>
 	);
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,25 @@
-import React from "react";
-import "./header.css";
-import CTA from "./CTA";
-import ME from "../../assets/me.png";
-import HeatherSocials from "./HeatherSocials";
+import React from 'react'
+import './Header.css'
+import PRIN from '../../assets/principal-transformed.png'
 
-function Header() {
-	return (
-		<header>
-			<div className="container header__container">
-				<div data-aos="fade-down">
-					<h5>Hello I'm</h5>
-					<h1>Edu</h1>
-					<h5 className="text-light">Data scientist</h5>
-				</div>
-				<div data-aos="fade-down" data-aos-duration="1000">
-					<CTA />
-				</div>
-				<HeatherSocials />
-
-				<div className="me">
-					<img src={ME} alt="" />
-				</div>
-
-				<a href="#contact" className="scroll__down">
-					Scroll Down
-				</a>
-			</div>
-		</header>
-	);
+const Header = () => {
+  return (
+    <div>
+      {/* IMAGEN */}
+      <div className='container-xl position-relative'>
+        <img aria-hidden="true" width="2236" height="1630" className='events-none position-absolute top-0 height-auto' src={PRIN} alt="" style={{width: "min(1118px, max(100vh, 100vw))", left: "67%;"}}/>
+        <img alt="" aria-hidden="true" width="860" height="544" class="events-none d-sm-none width-full height-auto mb-n3" src="https://github.githubassets.com/assets/hero-mobile-7163f4f5de41.webp"></img>
+      </div>
+      {/* NOMBRE */}
+      <div className="px-3">
+        
+      </div>
+      <h1 className='position-relative'>
+        <span className='title'>Eduard Perez Mendez</span>
+      </h1>
+      <h1>TEST</h1>
+    </div>
+  )
 }
 
-export default Header;
+export default Header
