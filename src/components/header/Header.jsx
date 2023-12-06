6abbc5728e94b4d57f044ba24563662a10df5820
Viper <eduardperezmendez@gmail.com>
2023-12-06 11:57:43 +0100

    Update package-lock.json and package.json

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,6 @@ const Header = () => {
       {/* IMAGEN */}
       <div className='container-xl position-relative'>
         <img aria-hidden="true" width="2236" height="1630" className='events-none position-absolute top-0 height-auto' src={PRIN} alt="" style={{width: "min(1118px, max(100vh, 100vw))", left: "67%;"}}/>
-        <img alt="" aria-hidden="true" width="860" height="544" class="events-none d-sm-none width-full height-auto mb-n3" src="https://github.githubassets.com/assets/hero-mobile-7163f4f5de41.webp"></img>
       </div>
       {/* NOMBRE */}
       <div className="px-3">
