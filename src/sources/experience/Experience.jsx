dbd23a46e1a3d9099fc7d203bf9c26ec1247080a
Viper <eduardperezmendez@gmail.com>
2023-12-06 12:35:41 +0100

    Remove unnecessary data attributes

--- src/sources/experience/Experience.jsx
+++ src/sources/experience/Experience.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Experience() {
   return (
-    <div className='home-campaign-security js-section overflow-hidden px-3 pb-8 pt-5' data-color-mode={dark} data-dark-theme={dark}>
+    <div className='home-campaign-security js-section overflow-hidden px-3 pb-8 pt-5'>
       <div className="container-xl">
         
       </div>
